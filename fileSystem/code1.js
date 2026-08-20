f19cf68d2dd98c6e322bd6e9d4109b19f2f34a0e
Ganesh Dutt <gdutt2304@gmail.com>
2026-08-20 12:12:59 +0530

    file System

---
+++ fileSystem/code1.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+// fs.readFile("task.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log("data :  ", data);
+// });
+
+// try {
+//   const data = fs.readFileSync("task.txt", "utf-8");
+//   console.log("data :  ", data);
+// } catch (err) {
+//   console.error("Error: ", err);
+// }
+
+// try {
+//   fs.writeFileSync("file1.txt", "hello node.js");
+//   console.log("file created successfully  ");
+// } catch (err) {
+//   console.error("Error: ", err);
+// }
+
+// try {
+//   fs.writeFileSync("file1.txt", "writeFileSync ");
+//   console.log("file created successfully  ");
+// } catch (err) {
+//   console.error("Error: ", err);
+// }
+
+// fs.writeFile("example.txt", "Node.js", (err) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log("file created successfully  ");
+// });
