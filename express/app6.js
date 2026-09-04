6bc37217ff5190a4cf3d9696bb13aa82909826e8
Ganesh Dutt <gdutt2304@gmail.com>
2026-09-04 12:40:23 +0530

    query

---
+++ express/app6.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const mobiles = [
+  { id: 1, brand: "Apple", color: "Black" },
+  { id: 2, brand: "Samsung", color: "White" },
+  { id: 3, brand: "OnePlus", color: "Red" },
+  { id: 4, brand: "Google", color: "Blue" },
+  { id: 5, brand: "Xiaomi", color: "Green" },
+];
+app.get("/api/mobiles", (req, res) => {
+  const { brand } = req.query;
+
+  const selectBrand = Array.isArray(brand) ? brand : [brand];
+
+  const filterBrand = mobiles.filter((m) => {
+    selectBrand.includes(m.brand);
+  });
+
+  res.json(filterBrand);
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
+// multi value  =  []
